3cad88dd525ca0482df58a2dc9793cd9e6bd63f6
Aleksandr M <alfie.mir@gmail.com>
2021-08-31 20:43:20 +0300

    Добавил несколько констант с типами

--- src/lectures/part1basics/ValVarTypes.scala
+++ src/lectures/part1basics/ValVarTypes.scala
@@ -1,12 +1,27 @@
 package lectures.part1basics
 
 object ValVarTypes extends App {
-  val x = 2.2;
+  var x = 2.2;
+  var y = x;
+
+  x = 45;
   println(x);
+  println(y);
+  println(y == x);
 
   def hello(name: String, surname: String): Unit = {
     println(s"Hello $name, $surname")
-  }
+  };
 
-  hello("Вася", "Пупкин")
+  hello("Вася", "Пупкин");
+
+  val aBoolean: Boolean = false;
+  val aChar: Char = 'a';
+  val aShort: Short = 15000;
+  var aInt: Int = 2147483647;
+  val aLong: Long = 546_546_541_325_465_463L;
+  val aFloat: Float = 1.232323f;
+  val aDouble: Double = 1.232323;
+
+  println(aLong)
 }
